c0911676f2ceadb6f3f4e204b4bf4a523f6c5766
jefferyooi <ooizhihern123@gmail.com>
2022-12-24 13:59:16 +0800

    added link to events page from orders pgae

--- Admin/orders.php
+++ Admin/orders.php
@@ -69,7 +69,7 @@ include "dbConnection.php"
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="events.html">
                         <span class="icon">
                             <ion-icon name="sparkles-outline"></ion-icon>
                         </span>
