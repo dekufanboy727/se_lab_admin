f3e6efc4dd33412182e35d4be419295e0266d5aa
DKoay0712 <73596089+kyubi5@users.noreply.github.com>
2022-12-29 01:53:12 +0800

    Add files via upload

---
+++ User/profile.php
@@ -0,0 +1,53 @@
+<?php
+    session_start();
+    include 'dbConnection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home_style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Marhey:wght@300;400&display=swap" rel="stylesheet">
+    <title>Helf Coffee Official Website</title>
+
+    <style>
+     
+
+    </style>   
+
+
+</head>
+<body>
+    <div class="nav_bar">
+        <div class="logo">
+            <a href="index.php"><img src="images/helf_coffee_logo.png" alt="Helf Coffee Logo" style="width: 130px" href="index.html"></a>
+        </div>
+
+        <?php if(!isset($_SESSION['logged_in'])) : ?>
+        <a href="user_login.php" class="login">Login</a>
+        <?php endif ?>
+
+        <?php if(isset($_SESSION['logged_in'])) : ?>
+        <a href="index.php?status=loggedout " class="login">Logout</a>
+        <a href="profile.php" class="login">Profile</a> 
+        <?php endif ?>
+
+        <nav class="pages">
+            <ul>
+                <li><a href="#">About Us</a></li>
+                <li><a href="menu_best_seller.php">Menu</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>        
+    </div>
+
+    </div>
+
+
+    
+</body>
+</html>
