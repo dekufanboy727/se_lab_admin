d554b7c62b7d102a649b26ff14e8b44ca302458f
DKoay0712 <73596089+kyubi5@users.noreply.github.com>
2022-12-30 01:09:15 +0800

    Update menu_beverages.php

--- User/menu_beverages.php
+++ User/menu_beverages.php
@@ -17,7 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/menu_style.css">
     <link href="https://fonts.googleapis.com/css2?family=Marhey:wght@300;400&display=swap" rel="stylesheet">
-    <script src="js/menu.js"></script>
+    <!-- <script src="js/menu.js"></script> -->
     <title>Helf Coffee Official Website</title>
 </head>
 <body>
@@ -56,155 +56,131 @@
         </ul>
     </div>
 
+
     <div class="item_container">
+
+    <?php
+        $sql = $conn->query("SELECT * FROM product WHERE product_type = 3 ORDER by product_id");
+
+        if($sql->num_rows > 0 ){
+
+            while($data=$sql->fetch_assoc()){
+
+        $cold = 0.40;        
+
+    ?>    
+
         <div class="item">
+                
             <div class="item_pic">
-                <img src="images/kopi_bancuh.jpg" style="bottom: 120px;">
+            <?php
+                echo  '<img style="bottom: '.$data['pixel'].'px;" src="data:image/jpeg;base64,'.base64_encode( $data['product_img'] ).'"/>';
+            ?>
             </div>
             <div class="description">
-                <div class="item_name"><center>Kopi Bancuh</center></div>
+                <div class="item_name"><center><?php echo $data['product_name']?></center></div>
                 <div class="price">
                     <div class="price">
-                        <center>RM5.50</center>
+                        <center>RM<?php echo $data['price']?>0</center>
                         <div class="sub_price"><center>Hot</center></div>
                     </div>
                     <div class="price">
-                        <center>RM5.90</center>
+                        <center>RM<?php echo $data['price']+$cold?>0</center>
                         <div class="sub_price"><center>Cold</center></div>
                     </div>
                 </div>
-                <div class="order_btn" onclick="togglePopup_1()"><center>Order Now</center></div>
+                <div class="order_btn" onclick="togglePopup_<?php echo $data['product_id']?>()"><center>Order Now</center></div>
             </div>
         </div>
-        <div class="item">
-            <div class="item_pic">
-                <img src="images/teh_bancuh.jpg" style="bottom: 175px;">
-            </div>
-            <div class="description">
-                <div class="item_name"><center>Teh Bancuh</center></div>
-                <div class="price">
-                    <div class="price">
-                        <center>RM5.50</center>
-                        <div class="sub_price"><center>Hot</center></div>
-                    </div>
-                    <div class="price">
-                        <center>RM5.90</center>
-                        <div class="sub_price"><center>Cold</center></div>
-                    </div>
-                </div>
-                <div class="order_btn" onclick="togglePopup_2()"><center>Order Now</center></div>
-            </div>
-        </div>
-        <div class="item">
-            <div class="item_pic">
-                <img src="images/kopi_teh.jpg" style="bottom: 0px;">
-            </div>
-            <div class="description">
-                <div class="item_name"><center>Kopi & Teh</center></div>
-                <div class="price">
-                    <div class="price">
-                        <center>RM5.50</center>
-                        <div class="sub_price"><center>Hot</center></div>
-                    </div>
-                    <div class="price">
-                        <center>RM5.90</center>
-                        <div class="sub_price"><center>Cold</center></div>
-                    </div>
-                </div>
-                <div class="order_btn" onclick="togglePopup_3()"><center>Order Now</center></div>
-            </div>
-        </div>
-        <div class="item">
-            <div class="item_pic">
-                <img src="images/cappuccino.jpg" style="bottom: 0px;">
-            </div>
-            <div class="description">
-                <div class="item_name"><center>Cappuccino</center></div>
-                <div class="price">
-                    <center>RM6.90</center>
-                    <div class="sub_price"><center>Hot</center></div>
-                </div>
-                <div class="price">
-                    <center>RM7.90</center>
-                    <div class="sub_price"><center>Cold</center></div>
-                </div>
-                <div class="order_btn" onclick="togglePopup_4()"><center>Order Now</center></div>
-            </div>
-        </div>
-        <div class="item">
-            <div class="item_pic">
-                <img src="images/latte.jpg" style="bottom: 100px;">
-            </div>
-            <div class="description">
-                <div class="item_name"><center>Latte</center></div>
-                <div class="price">
-                    <center>RM6.90</center>
-                    <div class="sub_price"><center>Hot</center></div>
-                </div>
-                <div class="price">
-                    <center>RM7.90</center>
-                    <div class="sub_price"><center>Cold</center></div>
-                </div>
-                <div class="order_btn" onclick="togglePopup_5()"><center>Order Now</center></div>
-            </div>
-        </div>
-        <div class="item">
-            <div class="item_pic">
-                <img src="images/mocha.jpg" style="bottom: 200px;">
-            </div>
-            <div class="description">
-                <div class="item_name"><center>Mocha</center></div>
-                <div class="price">
-                    <center>RM6.90</center>
-                    <div class="sub_price"><center>Hot</center></div>
-                </div>
-                <div class="price">
-                    <center>RM7.90</center>
-                    <div class="sub_price"><center>Cold</center></div>
-                </div>
-                <div class="order_btn" onclick="togglePopup_6()"><center>Order Now</center></div>
-            </div>
-        </div>
+
+        <?php
+           }
+        }    
+        ?>
+
     </div>
-    
-    <div class="popup" id="popup-1">
+    <?php 
+        $cold = 0.40; 
+        $sql = $conn->query("SELECT * FROM product WHERE product_type = 3 ORDER by product_id");
+        if($sql->num_rows > 0 ){
+
+            while($data=$sql->fetch_assoc()){
+    ?>
+    <div class="popup" id="popup-<?php echo $data['product_id']?>">
         <div class="overlay"></div>
         <div class="content">
-            <img src="images/kopi_bancuh.jpg">
+            <?php
+                echo  '<img src="data:image/jpeg;base64,'.base64_encode( $data['product_img'] ).'"/>';
+            ?>
             <div class="description_container">
                 <div class="description2">
-                    <div class="item_name2">Kopi Bancuh</div>
-                    <P>“Gu Mor Kak” or Demon Cow’s Horn Biscuit is a chinese homemade traditional biscuit that is packed with savory rosated chicken fillings, with a thin layered crust wrapped around it. It’s crunchy textute and salty with a hint a sweetness flavour is exactly why Gu Mor Kak is one of our cafe’s signature pastries and best seller.
+                    <div class="item_name2"><?php echo $data['product_name']?></div>
+                    <P><?php echo $data['product_desc']?>
                     </P>
                     <div class="deco">
                         <div class="deco_dot"></div>
                         <div class="deco_dot"></div>
                         <div class="deco_bar"></div>
                     </div>
-                    <div class="price_quantity_container">
-                        <div class="price">
-                            <div class="price2">
-                                <center>RM5.50</center>
-                                <div class="sub_price2"><center>Hot</center></div>
+                    <form>
+                        <div class="price_quantity_container">
+                            
+                            <div class="price">
+                                <div class="price2">
+                                    <center>RM<?php echo $data['price']?>0</center>
+                                    <div class="sub_price2"><center>Hot</center></div>
+                                </div>
+                                <div class="price2">
+                                    <center>RM<?php echo $data['price']+$cold?>0</center>
+                                    <div class="sub_price2"><center>Cold</center></div>
+                                </div>
                             </div>
-                            <div class="price2">
-                                <center>RM5.90</center>
-                                <div class="sub_price2"><center>Cold</center></div>
+                            <div class="quantity">
+                                <span class="minus"id="minus">-</span>
+                                <span class="num"id="num">01</span>
+                                <span class="plus" id="plus">+</span>
                             </div>
                         </div>
-                        <div class="quantity">
-                            <span class="minus"id="minus">-</span>
-                            <span class="num"id="num">01</span>
-                            <span class="plus" id="plus">+</span>
-                        </div>
-                    </div>
-                    <div class="add_to_cart_btn">Add To Cart</div>
+                        <div class="add_to_cart_btn">Add To Cart</div>
+                    </form>
                 </div>
             </div>
-            <div class="close_btn" onclick="togglePopup_1()">Close</div>
+            <div class="close_btn" onclick="togglePopup_<?php echo $data['product_id']?>()">Close</div>
         </div>
     </div>
+    <script>
+        function togglePopup_<?php echo $data['product_id']?>(){
+        document.getElementById("scroll").scrollIntoView();
+        document.getElementById("popup-<?php echo $data['product_id']?>").classList.toggle("active");
 
+        let plus = document.getElementById("plus");
+        let num = document.getElementById("num");
+        let minus = document.getElementById("minus");
+
+        let a = 0;
+
+        plus.addEventListener("click", ()=>{
+            a++;
+            a = (a<10)?"0"+a:a;
+            num.innerText = a;
+        });
+
+        minus.addEventListener("click", ()=>{
+            if(a>1){
+                a--;
+                a = (a<10)?"0"+a:a;
+                num.innerText = a;
+            }
+    });
+}
+    
+    </script>
+
+
+    <?php 
+            }
+        }
+    ?>
     <div class="popup" id="popup-2">
         <div class="overlay"></div>
         <div class="content">
