87d7e9c48f9556a0f9a26d7449eef616669d0ac0
jefferyooi <ooizhihern123@gmail.com>
2022-12-29 13:16:35 +0800

    update

--- Admin/event_add.php
+++ Admin/event_add.php
@@ -14,7 +14,7 @@ include "dbConnection.php"
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Events Update</title>
+    <title>Add Event</title>
     <link rel="icon" type="image/jpg" href="images/profile.jpg">
     <link rel="stylesheet" href="css/events_update.css">
 </head>
