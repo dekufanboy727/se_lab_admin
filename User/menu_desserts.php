7d072c3468fee7676794503f00e5527175448d9d
zhihong14 <121031279+zhihong14@users.noreply.github.com>
2022-12-30 17:07:38 +0800

    Update menu_desserts.php

--- User/menu_desserts.php
+++ User/menu_desserts.php
@@ -58,238 +58,111 @@
 
     <div class="item_container">
 
+    <?php
+        $sql = $conn->query("SELECT * FROM product WHERE product_type = 2 ORDER by product_id");
+
+        if($sql->num_rows > 0 ){
+
+            while($data=$sql->fetch_assoc()){
+
+
+    ?>    
         <div class="item">
+                
             <div class="item_pic">
-                <img src="images/peanut_ice_cream.jpg" style="bottom: 160px;">
+            <?php
+                echo  '<img style="bottom: '.$data['pixel'].'px;" src="data:image/jpeg;base64,'.base64_encode( $data['product_img'] ).'"/>';
+            ?>
             </div>
             <div class="description">
-                <div class="item_name"><center>Peanut Ice Cream</center></div>
+                <div class="item_name"><center><?php echo $data['product_name']?></center></div>
                 <div class="price">
-                    <center>RM4.30</center>
-                    <div class="sub_price"><center>for 3 scoops</center></div>
+                    <center>RM<?php echo number_format($data['price'], 2);?></center>
                 </div>
-                <div class="order_btn" onclick="togglePopup_1()"><center>Order Now</center></div>
+                <div class="order_btn" onclick="togglePopup_<?php echo $data['product_id']?>()"><center>Order Now</center></div>
             </div>
         </div>
 
-        <div class="item">
-            <div class="item_pic">
-                <img src="images/oreo_ice_cream.jpg" style="bottom: 160px;">
-            </div>
-            <div class="description">
-                <div class="item_name"><center>Oreo Ice Cream</center></div>
-                <div class="price">
-                    <center>RM4.30</center>
-                    <div class="sub_price"><center>for 3 scoops</center></div>
-                </div>
-                <div class="order_btn" onclick="togglePopup_2()"><center>Order Now</center></div>
-            </div>
-        </div>
-
-        <div class="item">
-            <div class="item_pic">
-                <img src="images/matcha_ice_cream.jpg" style="bottom: 160px;">
-            </div>
-            <div class="description">
-                <div class="item_name"><center>Matcha Ice Cream</center></div>
-                <div class="price">
-                    <center>RM5.30</center>
-                    <div class="sub_price"><center>for 3 scoops</center></div>
-                </div>
-                <div class="order_btn" onclick="togglePopup_3()"><center>Order Now</center></div>
-            </div>
-        </div>
-
-        <div class="item">
-            <div class="item_pic">
-                <img src="images/Oreo_Cheesecake.jpg" style="bottom: 154px;">
-            </div>
-            <div class="description">
-                <div class="item_name"><center>Oreo Cheesecake</center></div>
-                <div class="price">
-                    <center>RM13.00</center>
-                    <div class="sub_price"><center>per piece</center></div>
-                </div>
-                <div class="order_btn" onclick="togglePopup_4()"><center>Order Now</center></div>
-            </div>
-        </div>
-
-        <div class="item">
-            <div class="item_pic">
-                <img src="images/longan_soya_pudding.jpg" style="bottom: 150px;">
-            </div>
-            <div class="description">
-                <div class="item_name"><center>Longan Soya Pudding</center></div>
-                <div class="price">
-                    <center>RM4.50</center>
-                    <div class="sub_price"><center>per piece</center></div>
-                </div>
-                <div class="order_btn" onclick="togglePopup_5()"><center>Order Now</center></div>
-            </div>
-        </div>
+        <?php
+           }
+        }    
+        ?>
 
     </div>
+    <?php 
+        $sql = $conn->query("SELECT * FROM product WHERE product_type = 2 ORDER by product_id");
+        if($sql->num_rows > 0 ){
+
+            while($data=$sql->fetch_assoc()){
+    ?>
+
+    <div class="popup" id="popup-<?php echo $data['product_id']?>">
+        <div class="overlay"></div>
+        <div class="content">
+            <?php
+                echo  '<img src="data:image/jpeg;base64,'.base64_encode( $data['product_img'] ).'"/>';
+            ?>
+            <div class="description_container">
+                <div class="description2">
+                    <div class="item_name2"><?php echo $data['product_name']?></div>
+                    <P><?php echo $data['product_desc']?>
+                    </P>
+                    <div class="deco">
+                        <div class="deco_dot"></div>
+                        <div class="deco_dot"></div>
+                        <div class="deco_bar"></div>
+                    </div>
+                    <form>
+                        <div class="price_quantity_container">
+                            
+                            <div class="price2">
+                                <center>RM<?php echo number_format($data['price'], 2);?></center>
+                            </div>
+                            <div class="quantity">
+                                <span class="minus"id="minus<?php echo $data['product_id']?>">-</span>
+                                <span class="num"id="num<?php echo $data['product_id']?>">01</span>
+                                <span class="plus" id="plus<?php echo $data['product_id']?>">+</span>
+                            </div>
+                        </div>
+                        <div class="add_to_cart_btn">Add To Cart</div>
+                    </form>
+                </div>
+            </div>
+            <div class="close_btn" onclick="togglePopup_<?php echo $data['product_id']?>()">Close</div>
+        </div>
+    </div>
+    <script>
+        function togglePopup_<?php echo $data['product_id']?>(){
+        document.getElementById("scroll").scrollIntoView();
+        document.getElementById("popup-<?php echo $data['product_id']?>").classList.toggle("active");
+
+        let plus = document.getElementById("plus<?php echo $data['product_id']?>");
+        let num = document.getElementById("num<?php echo $data['product_id']?>");
+        let minus = document.getElementById("minus<?php echo $data['product_id']?>");
+
+        let a = 0;
+
+        plus.addEventListener("click", ()=>{
+            a++;
+            a = (a<10)?"0"+a:a;
+            num.innerText = a;
+        });
+
+        minus.addEventListener("click", ()=>{
+            if(a>1){
+                a--;
+                a = (a<10)?"0"+a:a;
+                num.innerText = a;
+            }
+    });
+}
     
-    <div class="popup" id="popup-1">
-        <div class="overlay"></div>
-        <div class="content">
-            <img src="images/peanut_ice_cream.jpg">
-            <div class="description_container">
-                <div class="description2">
-                    <div class="item_name2">Peanut Ice Cream</div>
-                    <P>“Gu Mor Kak” or Demon Cow’s Horn Biscuit is a chinese homemade traditional biscuit that is packed with savory rosated chicken fillings, with a thin layered crust wrapped around it. It’s crunchy textute and salty with a hint a sweetness flavour is exactly why Gu Mor Kak is one of our cafe’s signature pastries and best seller.
-                    </P>
-                    <div class="deco">
-                        <div class="deco_dot"></div>
-                        <div class="deco_dot"></div>
-                        <div class="deco_bar"></div>
-                    </div>
-                    <div class="price_quantity_container">
-                        <div class="price2">
-                            <center>RM4.30</center>
-                            <div class="sub_price2"><center>for 3 scoops</center></div>
-                        </div>
-                        <div class="quantity">
-                            <span class="minus" id="minus">-</span>
-                            <span class="num" id="num">01</span>
-                            <span class="plus" id="plus">+</span>
-                        </div>
-                        </div>
-                    <div class="add_to_cart_btn">Add To Cart</div>
-                    </div>
-                 </div>
-                <div class="close_btn" onclick="togglePopup_1()">Close</div>
-            </div>
-        </div>
-    </div>
-    
-    <div class="popup" id="popup-2">
-        <div class="overlay"></div>
-        <div class="content">
-            <img src="images/oreo_ice_cream.jpg">
-            <div class="description_container">
-                <div class="description2">
-                    <div class="item_name2">Oreo Ice Cream</div>
-                    <P>“Gu Mor Kak” or Demon Cow’s Horn Biscuit is a chinese homemade traditional biscuit that is packed with savory rosated chicken fillings, with a thin layered crust wrapped around it. It’s crunchy textute and salty with a hint a sweetness flavour is exactly why Gu Mor Kak is one of our cafe’s signature pastries and best seller.
-                    </P>
-                    <div class="deco">
-                        <div class="deco_dot"></div>
-                        <div class="deco_dot"></div>
-                        <div class="deco_bar"></div>
-                    </div>
-                    <div class="price_quantity_container">
-                        <div class="price2">
-                            <center>RM4.30</center>
-                            <div class="sub_price2"><center>for 3 scoops</center></div>
-                        </div>
-                        <div class="quantity">
-                            <span class="minus"id="minus-2">-</span>
-                            <span class="num"id="num-2">01</span>
-                            <span class="plus" id="plus-2">+</span>
-                        </div>
-                    </div>
-                    <div class="add_to_cart_btn">Add To Cart</div>
-                </div>
-            </div>
-            <div class="close_btn" onclick="togglePopup_2()">Close</div>
-        </div>
-    </div>
+    </script>
 
-    <div class="popup" id="popup-3">
-        <div class="overlay"></div>
-        <div class="content">
-            <img src="images/matcha_ice_cream.jpg">
-            <div class="description_container">
-                <div class="description2">
-                    <div class="item_name2">Matcha Ice Cream</div>
-                    <P>Homemade Portuguese style egg tart baked with an outer layer of crust, fragant egg fillings and a layer of burnt cheese on top. It’s aromatic, sweet and satly fillings combined with the crusty outer layer is definately a must try.
-                    </P>
-                    <div class="deco">
-                        <div class="deco_dot"></div>
-                        <div class="deco_dot"></div>
-                        <div class="deco_bar"></div>
-                    </div>
-                    <div class="price_quantity_container">
-                        <div class="price2">
-                            <center>RM5.30</center>
-                            <div class="sub_price2"><center>per cup</center></div>
-                        </div>
-                        <div class="quantity">
-                            <span class="minus"id="minus-3">-</span>
-                            <span class="num"id="num-3">01</span>
-                            <span class="plus" id="plus-3">+</span>
-                        </div>
-                    </div>
-                    <div class="add_to_cart_btn">Add To Cart</div>
-                </div>
-            </div>
-            <div class="close_btn" onclick="togglePopup_3()">Close</div>
-        </div>
-    </div>
-
-    <div class="popup" id="popup-4">
-        <div class="overlay"></div>
-        <div class="content">
-            <img src="images/Oreo_Cheesecake.jpg">
-            <div class="description_container">
-                <div class="description2">
-                    <div class="item_name2">Oreo Cheesecake</div>
-                    <P>Served fresh of the fridge with butter and oreo crumps as the base, special homemade cream cheese and milk recipe as the middle layer and top it off with oreo poweder sprinkles and a piece of oreo biscuit. Oreo lovers what are you waiting for? Try it now.
-                    </P>
-                    <div class="deco">
-                        <div class="deco_dot"></div>
-                        <div class="deco_dot"></div>
-                        <div class="deco_bar"></div>
-                    </div>
-                    <div class="price_quantity_container">
-                        <div class="price2">
-                            <center>RM13.00</center>
-                            <div class="sub_price2"><center>per piece</center></div>
-                        </div>
-                        <div class="quantity">
-                            <span class="minus"id="minus-4">-</span>
-                            <span class="num"id="num-4">01</span>
-                            <span class="plus" id="plus-4">+</span>
-                        </div>
-                    </div>
-                    <div class="add_to_cart_btn">Add To Cart</div>
-                </div>
-            </div>
-            <div class="close_btn" onclick="togglePopup_4()">Close</div>
-        </div>
-    </div>
-
-    <div class="popup" id="popup-5">
-        <div class="overlay"></div>
-        <div class="content">
-            <img src="images/longan_soya_pudding.jpg">
-            <div class="description_container">
-                <div class="description2">
-                    <div class="item_name2">Longan Soya Pudding</div>
-                    <P>“Gu Mor Kak” or Demon Cow’s Horn Biscuit is a chinese homemade traditional biscuit that is packed with savory rosated chicken fillings, with a thin layered crust wrapped around it. It’s crunchy textute and salty with a hint a sweetness flavour is exactly why Gu Mor Kak is one of our cafe’s signature pastries and best seller.
-                    </P>
-                    <div class="deco">
-                        <div class="deco_dot"></div>
-                        <div class="deco_dot"></div>
-                        <div class="deco_bar"></div>
-                    </div>
-                    <div class="price_quantity_container">
-                        <div class="price2">
-                            <center>RM4.50</center>
-                            <div class="sub_price2"><center>per piece</center></div>
-                        </div>
-                        <div class="quantity">
-                            <span class="minus"id="minus-5">-</span>
-                            <span class="num"id="num-5">01</span>
-                            <span class="plus" id="plus-5">+</span>
-                        </div>
-                    </div>
-                    <div class="add_to_cart_btn">Add To Cart</div>
-                </div>
-            </div>
-            <div class="close_btn" onclick="togglePopup_5()">Close</div>
-        </div>
-    </div>
+    <?php 
+            }
+        }
+    ?>
 
 </body>
 </html>
