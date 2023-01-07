8e9c0015260985108f41f0e2aaa97d2f7277a32d
DKoay0712 <73596089+kyubi5@users.noreply.github.com>
2023-01-08 07:00:24 +0800

    Update products.php

--- Admin/products.php
+++ Admin/products.php
@@ -34,7 +34,7 @@ include "dbConnection.php"
         {
             if(isset($_GET['product']) == true){
                 $deleteid = $_GET['product'];
-                $sql4 = "SELECT * FROM product WHERE id = '$deleteid'";
+                $sql4 = "SELECT * FROM product WHERE product_id = '$deleteid'";
                 $result_img = mysqli_fetch_assoc(mysqli_query($conn, $sql4));
                 unlink($result_img["product_img"]);
                 $sql3 = "DELETE FROM product WHERE product_id = '$deleteid'";
